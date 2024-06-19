b707b1bbb1d4fdbbff6a9044d36795b666d727b1
Mehul Behera <mbehera@umd.edu>
2024-06-19 14:25:32 -0400

    added search

--- server.ts
+++ server.ts
@@ -1,12 +1,49 @@
-import express from 'express';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
+const prisma = new PrismaClient();
 const PORT = 3000;
-const app = express()
+const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//get movie by id
+app.get("/findMovie/:name", async (req, res) => {
+  const movieName = req.params.name;
+
+  const movie = await prisma.movie.findUnique({
+    where: {
+      name: movieName,
+    },
+  });
+  console.log("movie: ", movie);
+  res.json(movie);
+});
+
+// //creates a favorite relationship between a user and movie
+// app.post("/createFav", async (req, res) => {
+//   const { movieID, userID } = req.params;
+
+//   const userFavMovie = await prisma.userOnMovies.create({
+//     data: {
+//       movieId: movieID,
+//       userId: userID,
+//     },
+//   });
+
+//   res.json(userFavMovie);
+// });
+
+//get list of all movies
+app.get("/findAllMovies", async (req, res) => {
+  const movies = await prisma.movie.findMany();
+  res.json(movies);
+});
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
